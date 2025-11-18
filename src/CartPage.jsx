0005fff0423ab6cd7e410e754017f3aa468e3a6c
vineet-lovanshi <vinitlovanshi9@gmail.com>
2025-11-18 13:19:27 +0530

    listing cart items

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CartPage = () => {
+  const cartSelector = useSelector((state) => state.cart.items);
+  // console.log(selector);
+
   return (
     <div>
-      <h1>Cart Page</h1>
+      <div className="cart-container">
+        <div className="cart-header">
+          <h2>Your Cart Items</h2>
+          <span>{cartSelector.length} items</span>
+        </div>
+        {cartSelector.length > 0
+          ? cartSelector.map((item) => (
+              <div className="cart-item" key={item.id}>
+                <div className="item-info">
+                  <img src={item.thumbnail} alt="" />
+                  <div className="item-details">
+                    <h4>{item.title}</h4>
+                    <p>{item.brand}</p>
+                  </div>
+                </div>
+                <div className="item-actions">
+                  <span className="price">$ {item.price}</span>
+                  <button className="btn">Remove</button>
+                </div>
+              </div>
+            ))
+          : null}
+        <div className="cart-footer">
+          Total Price : $
+          {cartSelector.reduce((sum, item) => sum + item.price, 0).toFixed(2)}
+        </div>
+      </div>
     </div>
   );
 };
